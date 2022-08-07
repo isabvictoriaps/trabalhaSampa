c481217e0caf39aadce420e19edb0aab33f4b109
Isabvictoriaps <isabvictoriaps@pop-os.localdomain>
2022-08-07 12:28:05 -0300

    tirando node

--- src/pages/Equipe.jsx
+++ src/pages/Equipe.jsx
@@ -8,7 +8,7 @@ import Footer from '../components/footer/Footer';
 function Equipe(){
     return (
        <div>
-            <NavbarLogin />
+            <Navbar />
             <InicioEquipe />
             <SlideVideo />
             <Footer />
